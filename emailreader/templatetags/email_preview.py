556f9dd7e94a09689e3351adc00f57db1dd16895
danxshap <registration@dshap.com>
2014-03-25 00:25:05 -0400

    show HTML email preview in an iframe

--- emailreader/templatetags/email_preview.py
+++ emailreader/templatetags/email_preview.py
@@ -1,3 +1,4 @@
+from base64 import b64encode
 from django import template
 from emailreader.models import DbEmailMessage
 
@@ -7,7 +8,9 @@ register = template.Library()
 @register.inclusion_tag('admin/emailreader/db_email_detail.html')
 def render_email_preview(object_id):
     from django.conf import settings
+    db_email_message = DbEmailMessage.objects.get(pk=object_id)
     return {
-        'object' : DbEmailMessage.objects.get(pk=object_id),
+        'object' : db_email_message,
+        'escaped_html': b64encode(db_email_message.body_html.encode('utf-8')),
         'MEDIA_URL': settings.MEDIA_URL,
     }
