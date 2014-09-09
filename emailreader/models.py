6ce788b37021964bd68318ba5fe51a3dd4ee9e3c
danxshap <registration@dshap.com>
2014-09-09 00:06:32 -0400

    default False for is_multipart

--- emailreader/models.py
+++ emailreader/models.py
@@ -4,15 +4,11 @@ from django.db import models
 class DbEmailMessage(models.Model):
 
     """docstring for EmailMessageMode"""
-        
+
     sent_at = models.DateTimeField()
     from_email = models.CharField(max_length=512)
     to = models.CharField(max_length=2024)
     subject = models.CharField(max_length=2024)
-    is_multipart = models.BooleanField()
+    is_multipart = models.BooleanField(default=False)
     body = models.TextField()
     body_html = models.TextField(blank=True, null=True)
-
-
-
-
